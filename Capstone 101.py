b58150c0f1d665f9246e5adf42f835c7a5601178
rahil1303 <67217341+rahil1303@users.noreply.github.com>
2020-07-10 16:02:34 +0530

    Week 1
    Applied Data science capstone week 1 Assignment.

---
+++ Capstone 101.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Applied Data Science Capstone: Notebook
+# Week 1 Assignment
+# 
+
+# In[4]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course")
+
+
+# In[ ]:
+
+
+
+
